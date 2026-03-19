eeec90dd43ae4a07f7745dac992377b091d10833
Gxelds3 <20253ds080@utez.edu.mx>
2026-03-18 19:54:27 -0600

    JavaFx agregar actualizar y eliminar

--- demoListViewFile/src/main/java/com/example/demolistviewfile/controllers/AppController.java
+++ demoListViewFile/src/main/java/com/example/demolistviewfile/controllers/AppController.java
@@ -17,12 +17,14 @@ public class AppController {
     private Label lblMsg;
     @FXML
     private ListView<String> listView;
+
     @FXML
-    private TextField txtName;
+    private TextField txtNombre;
+
     @FXML
     private TextField txtEmail;
     @FXML
-    private TextField txtAge;
+    private TextField txtEdad;
 
     private ObservableList<String> data = FXCollections.observableArrayList();
     PersonService service= new PersonService();
@@ -30,7 +32,18 @@ public class AppController {
     @FXML
     public void initialize(){
         listView.setItems(data);
+        listView.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    if (newValue != null) {
+                        String[] parts = newValue.split("-");
+                        txtNombre.setText(parts[0]);
+                        txtEmail.setText(parts[1]);
+                        txtEdad.setText(parts[2]);
+                    }
+                }
+        )  ;
         loadFromFile();
+
     }
 
     @FXML
@@ -39,38 +52,63 @@ public class AppController {
     }
 
     @FXML
-    public void onAddPerson() {
-        try {
-            String name = txtName.getText();
+    public void onAddPerson(){
+        try{
+            String name = txtNombre.getText();
             String email = txtEmail.getText();
-
-
-            int age = Integer.parseInt(txtAge.getText());
-
-
-            service.addPerson(name, email, age);
-
-            lblMsg.setText("Usuario ingresado correctamente");
-            lblMsg.setStyle("-fx-text-fill: green");
-
-
-            txtName.clear();
+            String edad = txtEdad.getText();
+            service.addPerson(name,email,edad);
+            lblMsg.setText("Usuario creado correctamente");
+            lblMsg.setStyle("-fx-text-alignment: green");
+            txtNombre.clear();
             txtEmail.clear();
-            txtAge.clear();
+            txtEdad.clear();
             loadFromFile();
+        }catch (IOException e){
+            lblMsg.setText("Es error de arhcivo"+ e.getMessage());
+            lblMsg.setStyle("-fx-text-alignment: green");
 
-        } catch (NumberFormatException e) {
-
-            lblMsg.setText("Error: La edad debe ser un número");
-            lblMsg.setStyle("-fx-text-fill: red");
-        } catch (IllegalArgumentException e) {
-
-            lblMsg.setText(e.getMessage());
-            lblMsg.setStyle("-fx-text-fill: red");
-        } catch (IOException e) {
-            lblMsg.setText("Error de archivo: " + e.getMessage());
-            lblMsg.setStyle("-fx-text-fill: red");
+        } catch (IllegalArgumentException e){
+            lblMsg.setText("Es error de datos" + e.getMessage());
+            lblMsg.setStyle("-fx-text-alignment: red");
         }
+    }
+    @FXML
+    public void onUpdate(){
+        try{
+            int index = listView.getSelectionModel().getSelectedIndex();
+            String name = txtNombre.getText();
+            String email = txtEmail.getText();
+            String edad = txtEdad.getText();
+            service.updatePerson(index,name,email,edad);
+            loadFromFile();
+            txtNombre.clear();
+            txtEmail.clear();
+            txtEdad.clear();
+            lblMsg.setText("Se Actualizo correctamente");
+        }catch (IOException e){
+            lblMsg.setText("Hubo un error en el archivo");
+        } catch (IllegalArgumentException error){
+            lblMsg.setText("Hubo un error con los datos" +error.getMessage());
+        }
+
+    }
+    @FXML
+    public void onDelete(){
+        try{
+            int index = listView.getSelectionModel().getSelectedIndex();
+            service.onDelete(index);
+            loadFromFile();
+            txtNombre.clear();
+            txtEmail.clear();
+            txtEdad.clear();
+            lblMsg.setText("Se Elimino correctamente");
+        }catch (IOException e){
+            lblMsg.setText("Hubo un error en el archivo");
+        } catch (IllegalArgumentException error){
+            lblMsg.setText("Hubo un error con los datos" +error.getMessage());
+        }
+
     }
 
     private void loadFromFile(){
--- demoListViewFile/src/main/java/com/example/demolistviewfile/services/PersonService.java
+++ demoListViewFile/src/main/java/com/example/demolistviewfile/services/PersonService.java
@@ -1,55 +1,79 @@
 package com.example.demolistviewfile.services;
 
-import com.example.demolistviewfile.repositories.PersonFileRepository;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import com.example.demolistviewfile.repositories.PersonFileRepository;
 
 public class PersonService {
-
     PersonFileRepository repo = new PersonFileRepository();
 
     public List<String> loadForListView() throws IOException {
-        List<String> lines = repo.readAllLines();
-        List<String> result= new ArrayList<>();
-        for(String line : lines){
-            if(line==null || line.isBlank()) continue;
+        List<String> lines = this.repo.readAllLines();
+        List<String> result = new ArrayList();
 
-            String[] parts= line.split(",");
-            String name=parts[0];
-            String email=parts[1];
-            result.add(name+"-"+email);
+        for(String line : lines) {
+            if (line != null && !line.isBlank()) {
+                String[] parts = line.split(",");
+                String name = parts[0];
+                String email = parts[1];
+                String edad = parts[2];
+                result.add(name + "-" + email + "-" + edad);
+            }
         }
+
         return result;
     }
 
-    public void addPerson(String name, String email, int age) throws IOException {
-        validate(name, email, age);
-
-
-        repo.addNewLine(name + "," + email + "," + age);
+    public void addPerson(String name, String email, String edad) throws IOException {
+        this.repo.addNewLine(name + "," + email+","+edad);
     }
 
-    private void validate(String name, String email, int age) {
+    public void updatePerson(int index, String name, String email, String edad) throws IOException {
 
-        if (name == null || name.isBlank() || name.length() < 3) {
-            throw new IllegalArgumentException("El nombre es incorrecto");
+        validate(name, email, Integer.parseInt(edad));
+        List<String > listaOriginal=repo.readAllLines();
+        List<String> cleanLines = new ArrayList<>();
+        for(String line : listaOriginal){
+
+            if(line!=null && !line.isBlank()){
+                cleanLines.add(line);
+
+            }
         }
+        cleanLines.set(index, name+","+email+","+edad);
+        repo.saveFile(cleanLines);
 
+    }
 
-        String em = (email == null) ? "" : email.trim();
-        if (em.isBlank() || !em.contains("@") || !em.contains(".")) {
-            throw new IllegalArgumentException("El email es inválido");
+    public void onDelete(int index) throws IOException {
+        List<String > listaOriginal=repo.readAllLines();
+        List<String> cleanLines = new ArrayList<>();
+        for(String line : listaOriginal){
+
+            if(line!=null && !line.isBlank()){
+                cleanLines.add(line);
+
+            }
         }
+        cleanLines.remove(index);
+        repo.saveFile(cleanLines);
+    }
 
 
-        if (age < 0) {
-            throw new IllegalArgumentException("La edad no puede ser negativa");
-        }
 
-        if (age < 18) {
-            throw new IllegalArgumentException("Solo aceptamos mayores de edad");
+    private void validate(String name, String email, int edad) {
+        if (name != null && !name.isBlank() && name.length() >= 3) {
+            String em = email == null ? "" : email.trim();
+            if (!em.isBlank() && em.contains("@") && em.contains(".")) {
+                if (edad <= 18 || edad < 0) {
+                    throw new IllegalArgumentException("la edad es incorrecto");
+                }
+            } else {
+                throw new IllegalArgumentException("El email no es valido");
+            }
+        } else {
+            throw new IllegalArgumentException("EL nombre es incorrecto");
         }
     }
 }
